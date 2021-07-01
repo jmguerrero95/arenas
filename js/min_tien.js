731805be17edddd740d295da50ec641137e16b03
Jose <josemanuelguerreroduarte@gmail.com>
2021-06-30 20:05:37 -0500

    first commit

---
+++ js/min_tien.js
@@ -0,0 +1,37 @@
+function cambio_1()
+{
+	document.getElementById("tien_princi").style.backgroundImage="url('imagenes/tienda2.jpg')"
+	document.getElementById("nodo_title").innerHTML="Hola1"
+	document.getElementById("nodo_texto").innerHTML="Como estas1"
+
+}
+function cambio_2()
+{
+	document.getElementById("tien_princi").style.backgroundImage="url('imagenes/tienda3.jpg')"
+	document.getElementById("nodo_title").innerHTML="Hola2"
+	document.getElementById("nodo_texto").innerHTML="Como estas2"		
+}
+function cambio_3()
+{
+	document.getElementById("tien_princi").style.backgroundImage="url('imagenes/tienda4.jpg')"
+	document.getElementById("nodo_title").innerHTML="Hola3"
+	document.getElementById("nodo_texto").innerHTML="Como estas3"
+}
+function cambio_4()
+{
+	document.getElementById("tien_princi").style.backgroundImage="url('imagenes/tienda5.jpg')"
+	document.getElementById("nodo_title").innerHTML="Hola4"
+	document.getElementById("nodo_texto").innerHTML="Como estas4"
+}
+function cambio_5()
+{
+	document.getElementById("tien_princi").style.backgroundImage="url('imagenes/tienda6.jpg')"
+	document.getElementById("nodo_title").innerHTML="Hola5"
+	document.getElementById("nodo_texto").innerHTML="Como estas5"
+}
+function cambio_6()
+{
+	document.getElementById("tien_princi").style.backgroundImage="url('imagenes/tienda7.jpg')"
+	document.getElementById("nodo_title").innerHTML="Hola6"
+	document.getElementById("nodo_texto").innerHTML="Como estas6"
+}
